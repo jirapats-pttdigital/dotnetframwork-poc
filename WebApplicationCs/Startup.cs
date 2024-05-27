42f3e7c59b1536ed124aec503f53d4d5aa88399d
anan rungmontreetanet <zanan.r@pttdigital.com>
2024-05-27 13:42:26 +0700

    commit code for test

---
+++ WebApplicationCs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationCs.Startup))]
+namespace WebApplicationCs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
